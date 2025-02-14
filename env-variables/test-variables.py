74fb1935063121d6654f1891ffea598851ce4e4a
trevorputbrese <trevor.putbrese@gmail.com>
2025-02-14 16:41:02 -0500

    tweaked variables

--- env-variables/test-variables.py
+++ env-variables/test-variables.py
@@ -27,9 +27,21 @@ print(embeddings_api_key)
 
 postgres_service = env.get_service(name="postgres")
 
-if postgres_service:
-    POSTGRES_JDBCURL = postgres_service.credentials.get("jdbcUrl")
-    POSTGRES_PASSWORD = postgres_service.credentials.get("password")
+postgres_service = env.get_service(name="postgres")
 
-print(POSTGRES_JDBCURL)
+postgres_service = env.get_service(name="postgres")
+
+if postgres_service:
+    raw_jdbc_url = postgres_service.credentials.get("jdbcUrl")
+    # Strip "jdbc:" prefix if it exists.
+    if raw_jdbc_url.startswith("jdbc:"):
+        POSTGRES_URL = raw_jdbc_url[5:]
+    else:
+        POSTGRES_URL = raw_jdbc_url
+    POSTGRES_PASSWORD = postgres_service.credentials.get("password")
+else:
+    POSTGRES_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:postgres@127.0.0.1:5432/postgres-db")
+    POSTGRES_PASSWORD = os.environ.get("DATABASE_PASSWORD", "postgres")
+
+print(POSTGRES_URL)
 print(POSTGRES_PASSWORD)
