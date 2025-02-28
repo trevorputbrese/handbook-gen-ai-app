f510489ae2587c0217a6c4735f84422aba3b025c
trevorputbrese <trevor.putbrese@gmail.com>
2025-02-28 09:31:20 -0500

    stop tracking extraneous folder

--- extraneous/database-schema.sh
+++
@@ -1,6 +0,0 @@
-CREATE EXTENSION vector;
-
-CREATE TABLE IF NOT EXISTS handbook_chunks (
-    id SERIAL PRIMARY KEY,
-    chunk_text TEXT NOT NULL,
-    embedding VECTOR(768)
--- extraneous/run-postgres.sh
+++
@@ -1,6 +0,0 @@
-docker run -e POSTGRES_USER=postgres \
-           -e POSTGRES_PASSWORD=postgres \
-           -e POSTGRES_DB=postgres-db \
-           --name postgres-container\
-           -p 5432:5432 \
-           -d ankane/pgvector
